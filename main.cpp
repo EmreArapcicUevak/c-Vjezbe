6db7d0a4250bce9830d120c80be5f15c353689e1
Jin Abdulov <JinAbdulov@gmail.com>
2021-09-10 23:36:42 +0200

    Zavrsetak sa vjezbom s klasama

--- main.cpp
+++ main.cpp
@@ -71,6 +71,7 @@ class Position {
 };
 */
 
+
 class PodesiBroj;
 
 class SakriveniBroj{
@@ -110,7 +111,149 @@ void SetX(int x , SakriveniBroj * Broj) {
 	*(Broj->x) = x;
 }
 
+
+class Point {
+	protected:
+		int x;
+	public:
+		Point(int x = 0) {
+			cout << "Konstructor iz point klase" << endl;
+			setX(x);
+		}
+		
+		~Point(){
+			cout << "Destructor iz point klase" << endl;
+		}
+		
+		int getX() {return x;}
+		
+		void setX (int x) {
+			this->x = x;
+		}
+};
+
+// PUBLIC -> Private - x , Protected - Protected , Public - Public
+// Protected -> Private - x , Protected - Protected , Public - Protected
+// Private -> Private - x , Protected - Private , Public - Private
+
+
+class Point2D : public Point 
+{
+	protected:
+		int y;
+	public:
+		Point2D(int x = 0 , int y = 0) : Point(x){
+			setY(y);
+			cout << "Konstructor iz point2D klase" << endl;
+		}
+		
+		~Point2D(){
+			cout << "Destructor iz point2D klase" << endl;
+		}
+		
+		void setY(int y) {
+			this->y = y;
+		}
+		
+		int getY() {return y;}
+		
+		
+		void setXY(int x , int y){
+			setX(x);
+			setY(y);
+		}
+		
+		
+		void getXY() {
+			cout << x << ", " << y << endl;
+		}
+};
+
+class Point3D : public Point2D {
+	protected:
+		int z;
+	public:
+		Point3D(int x = 0 , int y = 0 , int z = 0) : Point2D(x , y) {
+			setZ(z);
+			cout << "Konstructor iz point3D klase" << endl;
+		}
+		
+		~Point3D(){cout << "Destructor iz point3D klase" << endl;}
+		
+		void setZ (int z) {
+			this->z = z;
+		}
+		
+		void setXYZ (int x , int y , int z) {
+			setZ(z);
+			setXY(x , y);
+		}
+		
+		void getXYZ(){
+			cout << x << ", " << y << ", " << z << endl;
+		}
+};
+
+
+class Monster {
+	protected:
+		float Healt;
+		string Name;
+	public:
+		Monster(){}
+		virtual ~Monster(){
+			cout << "Destruktor iz klase Monster";
+		}
+		
+		virtual string getName() {return this->Name;}
+		virtual float getHealt() {return this->Healt;}
+};
+
+class Zombie : public Monster {
+	public:
+		Zombie() {
+			this->Name = "Zombie";
+			this->Healt = 58;
+		}
+		
+		~Zombie() {
+			cout << "Destruktor iz klase Zombie" << endl;
+		}
+	string getName() {return this->Name + 'x';}	
+};
+
+class X : public Zombie {
+	public:
+		
+		X() {
+				this->Name = "X";
+				this->Healt = 100;
+		}
+		
+		~X() {
+				cout << "Destruktor iz klase X" << endl;
+			}
+			
+	string getName() {return this->Name + 'z';}	
+};
+
+class Skeleton : public Monster{
+    public:
+        Skeleton(){
+            this->Name="Skeleton";
+            this->Healt=60;
+        }
+};
+
 int main(int argc, char** argv) {
+	
+	Monster *MP = new X;
+	
+	cout << MP->getName();
+	delete MP;
+	
+	//Point3D a(5);
+
 	//std::cout << test.x; ovo ne moze jer je x private
 	/*ID test2;
 	
@@ -136,6 +279,8 @@ int main(int argc, char** argv) {
 	SetX(20 , &sb); 
 	
 	*/
+	
+	/*
 	PodesiBroj::SetX(10 ,&sb);
 	
 	SakriveniBroj sb2(sb);
@@ -143,7 +288,7 @@ int main(int argc, char** argv) {
 	
 	sb.printX();
 	sb2.printX();
-	
+	*/
 	
 	return 0;
 }
