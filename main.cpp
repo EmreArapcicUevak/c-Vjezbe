d894225ecc89ec93140849b48b855ddceef2fb20
Jin Abdulov <JinAbdulov@gmail.com>
2021-09-14 22:19:22 +0200

    Zavrsetak sa ucenjem operator overloading

--- main.cpp
+++ main.cpp
@@ -3,49 +3,113 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	fstream myFileHandler;
-	
-	myFileHandler.open("test5.txt" ,  ios::out | ios::app | ios::binary);
-	
-	/*
-		DEFAULT MODE
-		fstream - ios::in | ios::out
+
+class Point {
+	protected:
+		int x;
+	public:
+		Point() {
+			this->x = 0;
+		}
+
+		Point(int x) {
+			setX(x);
+		}
+
+
+		Point(const Point &obj) {
+			setX(obj.x);
+		}
 		
-		otvorice se file AKO POSTOJI , i mozemo pisati i citati , i kursor je uvijek na pocetku
-	*/
-	
-	/*
-		INPUT MODE
-		fstream - ios::in
+		~Point(){};
+
+		int getX() const {return x;}
+		void setX(int x) {this->x = x;}
+};
+
+
+class Point2D : public Point {
+	protected:
+		int y;
+	public:
+		Point2D() : Point() {
+			this->y = 0;
+		}
+
+		Point2D(int x , int y) : Point(x) {
+			cout << "Init constructor called" << endl;
+			setY(y);
+		}
+
+		Point2D(const Point2D &obj) : Point(obj) {
+			cout << "Copy contructor called" << endl;
+			setY(obj.y);
+		}
+
+		~Point2D(){}
+
+		int getY() const {return y;}
+		void setY(int y) {this->y = y;}
+
+		void printXY() const {
+			cout << x << " , " << y << endl;
+		}
 		
-		Mozemo samo citati file
-	*/
-	
-	/*
-		OUTPUT MODE
-		fstream - ios::out
-		
-		Mozemo samo pisati , ako file NE postoji on ce ga napraviti , medzutim on truncate-a file
-	*/
-	
-	/*
-		ios::trunc -- brise sve iz file-a
-		ios::ate -- At The End - postavlja pointer na kraj fila-a
-		ios::app - Appen - nove stvari  stavlja na kraj file-a ALI nije moguce mijenjati ili brisati stvari prije kraja
-		ios::binary -- Otvori file kao binary file
-	*/
-	
-	if (myFileHandler.is_open()){
-		myFileHandler << "test\n";
-		myFileHandler.close();
-	} else {
-		cout << "File nije otvoren";
-	}
-	
-	
-	return 0;
+		void setXY(int x , int y) {
+			setX(x);
+			setY(y);
+		}
+
+		const Point2D &operator= (const Point2D &obj) {
+			cout << "Assignment running" << endl;
+			setXY(obj.x , obj.y);
+
+			return *this;
+		}
+
+		friend ostream &operator<<(ostream & , const Point2D &);
+		friend Point2D operator+(const Point2D& , const Point2D&);
+		friend fstream &operator<<(fstream & ,const Point2D&);
+};
+
+
+ostream &operator<<(ostream &out , const Point2D &obj) {
+			out << obj.x << " , " << obj.y;
+			return out;
 }
 
+fstream &operator<<(fstream &file ,const Point2D& obj) {
+	file << obj.x << " , " << obj.y;
+	return file;
+}
+Point2D operator+(const Point2D& obj1 , const Point2D& obj2) {
+	return Point2D(obj1.x + obj2.x , obj1.y + obj2.y);
+}
+
+int main(int argc, char** argv) {
+	ofstream fileHandler;
+	fileHandler.open("DotTest.txt");
 
 
+	Point2D prvaTacka(1 , 5);
+	Point2D drugaTacka = prvaTacka; // drugaTacka(prvaTacka)
+
+	drugaTacka.setY(2);
+
+	prvaTacka.setY(7);
+
+	drugaTacka = prvaTacka;
+	drugaTacka.printXY();
+	cout << drugaTacka << endl;
+
+	prvaTacka.setXY(5 , 5);
+	drugaTacka = prvaTacka;
+
+	prvaTacka = prvaTacka + drugaTacka + drugaTacka;
+
+	cout << prvaTacka << endl;
+	fileHandler << prvaTacka << " | " << drugaTacka; // isto kao operator<<(operator<<(operator<<(fileHandler , prvaTacka) , "|") , drugaTacka);
+	
+	fileHandler.close();
+	return 0;
+}
