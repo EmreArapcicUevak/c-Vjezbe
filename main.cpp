f7e58c8bcc7a2f7231c77e739af9bb2bcc8511ed
Jin Abdulov <JinAbdulov@gmail.com>
2021-09-16 16:58:52 +0200

    Zavrsetak vjezbe koristenjem itteratora

--- main.cpp
+++ main.cpp
@@ -1,59 +1,18 @@
 #include <iostream>
 
 using namespace std;
-template <class type>
-bool match(type someText) {
-	return someText.size() >= 3;
-}
-
-template <class type>
-bool match2(type someText) { 
-	return someText[0] == 'E';
-}
-
-template <class type>
-unsigned int Prebroji(type arr[] , unsigned int size , bool (*match)(type) ) {
-	unsigned int counter = 0;
-	
-	for (unsigned int i = 0 ; i < size ; i++) {
-		if (	match(arr[i])	)
-			counter++;
-	}
-
-	return counter;
-}
-
-void printHello() {
-	cout << "Hello" << endl;
-}
-
-
-
-struct MatchTest {
-	bool operator()(string &text) {
-		return text == "ab";
-	}
-};
-
-
 
 int main(int argc, char** argv) {
-	
-	MatchTest a;
-	
-	void (*pFunc)() = printHello;
-	pFunc();
+	string Test = "Emre i Muhammedaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
-	string TextArr[] = {"ab" , "abc" , "Emre" , "Muhammed" , "ab"};
-	
-	cout << "Imamo " << Prebroji(TextArr , 5 , match) << " stringova cija je velicina veca ili jednaka 3" << endl;
-	cout << "Imamo " << Prebroji(TextArr , 5 , match2) << " stringova koji pocinju sa velikim slovom E" << endl;
-	
-	for (unsigned int i = 0 ; i < 5 ; i++) {
-		if (	a(TextArr[i])	)
-			cout << "ab found" << endl;
+
+	for (unsigned int i = 0 ; i < Test.size() ; i++) {
+	cout << Test[i] << ' ';
 	}
 
+	for (string::iterator it = Test.begin() ; it != Test.end() ; it++) {
+		cout << *it << ' ';
+	}
 
 	return 0;
 }
