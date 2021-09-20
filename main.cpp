123961dd24b00dbb0708eee1b8e1bcea05bb42e0
Emre Arapcic-Uevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2021-09-20 21:57:43 +0200

    Zavrsili smo sa ques i popravili malo bugova

--- main.cpp
+++ main.cpp
@@ -42,6 +42,7 @@ class Stack {
         }
 
         Stack(myT Data) {
+            this->CurrentNode = NULL;
             Push(Data);
         }
 
@@ -89,6 +90,86 @@ class Stack {
         bool AtTheEnd() {return this->CurrentNode == NULL;}
 };
 
+
+template<class myT>
+class Que {
+    private:
+        Node<myT> *LastNode;
+        Node<myT> *FirstNode;
+
+        void CopyQue(const Que<myT> &objToCopy) {
+            Node<myT> *HelpNode = this->FirstNode;
+
+            while(HelpNode != NULL) {
+                Push(HelpNode->Data);
+            }
+        }
+
+        void DeleteQue() {
+            Node<myT> *DeleteNode;
+
+            while(this->FirstNode != NULL){
+                DeleteNode = this->FirstNode;
+                this->FirstNode = this->FirstNode->Next;
+
+                delete DeleteNode;
+            }
+        }
+    public:
+        Que() {
+            this->LastNode = this->FirstNode = NULL;
+        }
+
+        Que(myT Data) {
+            this->LastNode = this->FirstNode = NULL;
+            Push(Data);
+        }
+
+        Que(const Que<myT> &objToCopy) {
+            this->LastNode = this->FirstNode = NULL;
+            CopyQue(objToCopy);
+        }
+
+        ~Que() {
+            DeleteQue();
+        }
+        void Push(myT Data) { // O(1)
+            if (this->FirstNode == NULL) // 1
+            {
+                this->LastNode = this->FirstNode = new Node<myT>; // 3
+                this->FirstNode->Data = Data; // 1
+            }else {
+                this->LastNode->Next = new Node<myT>; // 2
+                this->LastNode = this->LastNode->Next; // 1
+                this->LastNode->Data = Data; // 1
+            }
+
+            this->LastNode->Next = NULL;// 1
+        }
+
+        myT Pop() {
+            if (this->FirstNode == NULL)
+                throw "No Que to pop from";
+
+            const myT ReturnData = this->FirstNode->Data;
+            Node<myT> *DeleteNode = this->FirstNode;
+            this->FirstNode = this->FirstNode->Next;
+
+            delete DeleteNode;
+            return ReturnData;
+        }
+
+
+        const Que<myT> &operator=(const Que<myT> &objToCopy) {
+            DeleteQue();
+            CopyQue(objToCopy);
+            return *this;
+        }
+
+        bool AtTheEnd() {return this->FirstNode == NULL;}
+};
+
+
 int main(int argc, char** argv) {
     try {
         Stack<int> Ocjene;
@@ -101,8 +182,17 @@ int main(int argc, char** argv) {
             cout << Ocjene.Pop() << endl;
 
         Stack<int> Augustus = Ocjene;
-        Augustus.Pop();
+        //Augustus.Pop();
         
+        Que<int> Ocjene2;
+
+        cout << endl;
+        Ocjene2.Push(1);
+        Ocjene2.Push(2);
+        Ocjene2.Push(3);
+
+        while (!Ocjene2.AtTheEnd())
+            cout << Ocjene2.Pop() << endl;
     }catch (const char *errorMsg) {
         cout << errorMsg;
     }
