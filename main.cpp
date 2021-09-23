6d578c478f15c738df62c4b9e244d55bbce47567
Emre Arapcic-Uevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2021-09-23 20:39:17 +0200

    Napravili smo insertion i selection sort

--- main.cpp
+++ main.cpp
@@ -18,6 +18,30 @@ class SorthingAlgorithm {
             }
         } while(sorting);
     }
+
+    static void selectionSort(myT *Arr , const unsigned int size , bool (*passFunc)(myT , myT)) {
+        myT selectedElementIndex;
+        for (unsigned int sortSpot = 0 ; sortSpot < size - 1; sortSpot++){
+            selectedElementIndex = sortSpot;
+            for (unsigned int i = sortSpot + 1 ; i < size ; i++) {
+                if (passFunc(Arr[selectedElementIndex] , Arr[i]))
+                    selectedElementIndex = i;
+            }
+
+            if (sortSpot != selectedElementIndex)
+                swap(Arr[sortSpot] , Arr[selectedElementIndex]);
+        }
+    }
+
+    static void insertionSort(myT *Arr , const unsigned int size , bool (*passFunc)(myT , myT)) {
+        for (unsigned int sortSpot = 1 ; sortSpot < size ; sortSpot++)
+            for (unsigned int i = sortSpot ; i > 0 ; i--)
+                if (passFunc(Arr[i-1] , Arr[i]))
+                    swap(Arr[i] , Arr[i-1]);
+                else
+                    break;
+    }
+
 };
 
 template<class myT>
@@ -43,10 +67,9 @@ void printArr(myT *Arr , const unsigned int size) {
 
 int main(int argc, char** argv) {
     try {
-        int Arr[] = {6 , 4 , 3 , 1};
+        int Arr[] = {1,5,2,3};
 
-        SorthingAlgorithm<int>::bubbleSort(Arr , 4 , HighToLow<int>);
-
+        SorthingAlgorithm<int>::insertionSort(Arr , 4 , LowToHigh<int>);
         printArr<int>(Arr , 4);
     }catch (const char *errorMsg) {
         cout << errorMsg;
